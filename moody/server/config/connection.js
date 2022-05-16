1de532bddb25758a5b1dd9b640da8101d1272792
njmariaqi <njmariaqi@gmail.com>
2022-05-16 18:46:02 -0500

    deploying

--- moody/server/config/connection.js
+++ moody/server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/moody', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Moody-App', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, err => {
