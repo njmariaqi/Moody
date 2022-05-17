9d9b0768b4264c33e7e5b5d27fdab5d604a526be
njmariaqi <njmariaqi@gmail.com>
2022-05-16 19:20:58 -0500

    deploy fix config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Moody-App', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/moody', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, err => {
